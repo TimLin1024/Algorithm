974159925868e7b896385a578e969d7374a13db3
ivanljt <596476418@qq.com>
2017-04-28 15:00:19 +0800

    upload test data

--- src/sorting/quicksort2_3/Quick3Way.java
+++ src/sorting/quicksort2_3/Quick3Way.java
@@ -6,33 +6,33 @@ import edu.princeton.cs.algs4.StdRandom;
 import fundamental.algorithm_analysis1_4.Stopwatch;
 
 public class Quick3Way {
-	
+
 	/**
-	 *     < v     = v -------未排序 ---  ---- > v 
-	 * lo  --      lt  --     i    --- gt   --    hi
+	 * < v = v -------未排序 --- ---- > v lo -- lt -- i --- gt -- hi
 	 * 
-	 * */
+	 */
 	private static void sort(Comparable[] a, int lo, int hi) {
 		if (hi <= lo) {
 			return;
 		}
 		int lt = lo;
-		int i = lo+1;
+		int i = lo + 1;
 		int gt = hi;
 		Comparable v = a[lo];
-		while (i<=gt) {
+		while (i <= gt) {
 			int cmp = a[i].compareTo(v);
 			if (cmp < 0) {
 				exch(a, i++, lt++);
-			}else if (cmp > 0) {
+			} else if (cmp > 0) {
 				exch(a, i, gt--);
-			}else {
+			} else {
 				i++;
 			}
-		}//现在 a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi] 成立
-		sort(a, lo, lt-1);
-		sort(a, gt+1, hi);
+		} // 现在 a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi] 成立
+		sort(a, lo, lt - 1);
+		sort(a, gt + 1, hi);
 	}
+
 	private static boolean less(Comparable v, Comparable w) {
 		return v.compareTo(w) < 0;
 	}
@@ -42,12 +42,12 @@ public class Quick3Way {
 		a[i] = a[j];
 		a[j] = tComparable;
 	}
-	
+
 	public static void sort(Comparable[] a) {
 		StdRandom.shuffle(a);
-		sort(a, 0, a.length-1);
+		sort(a, 0, a.length - 1);
 	}
-	
+
 	public static void main(String[] args) {
 		int MAX = 100;
 		Integer[] aIntegers = new Integer[MAX];
